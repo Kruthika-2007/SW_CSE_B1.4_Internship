eff11a53970838672592af43d49cfcd2847f89ca
Kruthika-2007 <kruthikananth30@gmail.com>
2026-07-30 13:19:53 +0530

    Hooks: ex2 useEffect hook

--- day14-react-app/src/App.jsx
+++ day14-react-app/src/App.jsx
@@ -1,18 +1,22 @@
-import "./App.css";
+import './App.css'
 import SimpleList from "./components/ListOne";
 import UserList from "./components/ListTwo";
 import ProductListDemo from "./components/ListThree";
 import CounterHook from "./components/HookOne";
+import UserListHook from "./components/HookTwo";
 
 function App() {
+  
   return (
     <>
-      <SimpleList />
+      {/* <SimpleList />
       <UserList />
       <ProductListDemo />
-      <CounterHook />
+
+      <CounterHook/> */}
+      <UserListHook/>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
