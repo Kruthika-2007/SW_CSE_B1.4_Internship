3788312cb9f5456029864925c187c0f23f3e843f
Kruthika-2007 <kruthikananth30@gmail.com>
2026-07-30 12:56:59 +0530

    Hooks: useState

--- day14-react-app/src/App.jsx
+++ day14-react-app/src/App.jsx
@@ -1,20 +1,18 @@
-import './App.css'
+import "./App.css";
 import SimpleList from "./components/ListOne";
 import UserList from "./components/ListTwo";
 import ProductListDemo from "./components/ListThree";
 import CounterHook from "./components/HookOne";
 
 function App() {
-  
   return (
     <>
       <SimpleList />
       <UserList />
       <ProductListDemo />
-
-      <CounterHook/>
+      <CounterHook />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
